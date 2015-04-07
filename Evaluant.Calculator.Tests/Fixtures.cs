cdb74f8a6ee6c469526046b28af745bb9a05e94e
pitermarx <pedrohmarques@gmail.com>
2015-04-07 15:20:11 +0100

    Added Performance Tests. Removed unused

--- Evaluant.Calculator.Tests/Fixtures.cs
+++ Evaluant.Calculator.Tests/Fixtures.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using NCalc.Domain;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading;
 using System.Collections;
+using ThreadState = System.Threading.ThreadState;
 
 namespace NCalc.Tests
 {
+    [TestClass]
+    public class PerformanceTests
+    {
+        public static void AssertTime(Action action, TimeSpan expected, int times = 100)
+        {
+            // warmup
+            action();
+
+            var stopwatch = Stopwatch.StartNew();
+            for (var i = 0; i < times; i += 1) action();
+            stopwatch.Stop();
+            Console.Write(stopwatch.Elapsed);
+            Assert.IsTrue(stopwatch.Elapsed <= expected);
+        }
+
+        [TestMethod]
+        public void ExpressionMixNoCachePerformanceTest()
+        {
+            var expressions = new[]
+            {
+                "Abs(-1) + Cos(2)",
+                "2 + 3 + 5",
+                "2 * 3 + 5",
+                "2 * (3 + 5)",
+                "2 * (2*(2*(2+1)))",
+                "10 % 3",
+                "true or false",
+                "not true",
+                "false || not (false and true)",
+                "3 > 2 and 1 <= (3-2)",
+                "3 % 2 != 10 % 3"
+            };
+
+            AssertTime(() => expressions.ToList().ForEach(e => new Expression(e, EvaluateOptions.NoCache).Evaluate()), TimeSpan.FromMilliseconds(500));
+        }
+    }
+
     [TestClass]
     public class Fixtures
     {
